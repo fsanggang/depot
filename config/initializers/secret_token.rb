9890fad6dcd5d962967ef546d48efd3cebfff378
Fiona Sanggang <fsanggang@gmail.com>
2013-11-02 10:16:31 +1300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cuteorsad::Application.config.secret_key_base = '2787a818651ecc3cafbe36b1e0843a4b08e6cc3892825db041b0e61cf4216af5e67ebae696e64fa2444dba338bdd318d25f87c23b70e3600a221dc1699f66b75'
