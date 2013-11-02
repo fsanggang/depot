c060c02c2c50cc8d93fd3a5ddeef64119e19ab0e
Fiona Sanggang <fsanggang@gmail.com>
2013-11-02 21:31:15 +1300

    Added validations for products, and added styling for error messages.
    modified:   app/models/product.rb
    modified:   app/views/products/_form.html.erb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :image_url, presence: true, allow_blank: true, format: {
+    with: /.(gif|jpg|png)/,
+    message: "must be a URL for a .gif, .jpg, or .png image."
+  }
 end
